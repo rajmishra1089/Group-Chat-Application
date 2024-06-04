97043fd77602c2766cfc2922914718b7a17ba77d
rajmishra1089 <rajmishra1089@gmail.com>
2024-06-05 00:10:43 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://group-chat-application-frontend.onrender.com",
     methods: ["GET", "POST"],
   },
 });
